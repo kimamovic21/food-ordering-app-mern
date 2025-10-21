93e25256bfc4b746a35cac107d0130947e77847c
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-21 16:22:29 +0200

    Mobile nav

---
+++ frontend/src/components/shared/MobileNav.tsx
@@ -0,0 +1,36 @@
+import { Menu } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger
+} from '../ui/sheet';
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className='text-orange-500' />
+      </SheetTrigger>
+
+      <SheetContent className='space-y-3'>
+        <SheetTitle>
+          <span>Welcome to MernEats.com</span>
+        </SheetTitle>
+
+        <Separator />
+
+        <SheetDescription className='flex'>
+          <Button className='flex-1 font-bold bg-orange-500'>
+            Login
+          </Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
