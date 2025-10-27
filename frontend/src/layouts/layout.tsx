c6c59ff0827c5503423832ae622d9140c49c2694
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-27 17:36:47 +0100

    User profile form

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -4,14 +4,18 @@ import Hero from '@/components/shared/Hero';
 
 type Props = {
   children: React.ReactNode;
+  showHero?: boolean;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout = ({
+  children,
+  showHero = false
+}: Props) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
 
-      <Hero />
+      {showHero && <Hero />}
 
       <div className='container mx-auto flex-1 py-10'>
         {children}
