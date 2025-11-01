3cc83bda4297a0b62d12f3d6aa24757b733c503a
Kerim Imamović <imamovic.kerim@gmail.com>
2025-11-01 15:28:46 +0100

    Search bar component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import SearchBar, { type SearchForm } from '@/components/shared/SearchBar';
 import landingImage from '../assets/landing.png';
 import appDownloadImage from '../assets/appDownload.png';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <section className='flex flex-col gap-12'>
-      <div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
+      <div className='md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
         <h1 className='text-5xl font-bold tracking-tight text-orange-600'>
           Tuck into a takeaway today!
         </h1>
@@ -12,10 +22,18 @@ const HomePage = () => {
         <span className='text-xl'>
           Food is just a click away
         </span>
+
+        <SearchBar
+          placeHolder='Search by city or town'
+          onSubmit={handleSearchSubmit}
+        />
       </div>
 
       <div className='grid md:grid-cols-2 gap-5'>
-        <img src={landingImage} alt='Landing image' />
+        <img
+          src={landingImage}
+          alt='Landing image'
+        />
 
         <div className='flex flex-col items-center justify-center gap-4 text-center'>
           <span className='font-bold text-3xl tracking-tighter'>
@@ -26,7 +44,10 @@ const HomePage = () => {
             Download the MernEats App for faster ordering and personalized recommendations
           </span>
 
-          <img src={appDownloadImage} alt='App Download image' />
+          <img
+            src={appDownloadImage}
+            alt='App Download image'
+          />
         </div>
       </div>
     </section>
