6d7e354317eacd68ca7c9f41961cbf33c35592fa
Kerim Imamović <imamovic.kerim@gmail.com>
2025-11-03 16:48:54 +0100

    Search page layout

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,18 +1,37 @@
 import { useParams } from 'react-router-dom';
 import { useSearchRestaurant } from '@/api/SearchRestaurantApi';
+import SearchResultInfo from '@/components/search/SearchResultInfo';
+import SearchResultCard from '@/components/search/SearchResultCard';
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results: restaurants } = useSearchRestaurant(city);
+  const {
+    results: restaurants,
+    isLoading,
+  } = useSearchRestaurant(city);
+
+  if (!restaurants?.data || !city) {
+    return <div>No restaurants found.</div>;
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  };
 
   return (
-    <section>
-      <span>User searched for {city}</span>
-      <div>
-        {restaurants?.data.map((restaurant) => (
-          <div key={restaurant._id}>
-            found - {restaurant.restaurantName}, {restaurant.city}
-          </div>
+    <section className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
+      <div id='cuisines-list'>
+        Insert cuisines here :)
+      </div>
+
+      <div id='main-content' className='flex flex-col gap-5'>
+        <SearchResultInfo
+          total={restaurants.pagination.total}
+          city={city}
+        />
+
+        {restaurants.data?.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
         ))}
       </div>
     </section>
