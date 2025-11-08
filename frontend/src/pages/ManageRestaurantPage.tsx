d60f2daa650174466729a4af645e7664eb3ed9bb
Kerim Imamović <imamovic.kerim@gmail.com>
2025-11-08 15:13:46 +0100

    Restaurant Orders page

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,10 +1,18 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
-  useUpdateMyRestaurant
+  useUpdateMyRestaurant,
+  useGetMyRestaurantOrders
 } from '../api/MyRestaurantApi';
-import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
+} from '@/components/ui/tabs';
 import type { Restaurant } from '@/types';
+import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm';
+import OrderItemCard from '@/components/orders/OrderItemCard';
 
 const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
@@ -18,6 +26,11 @@ const ManageRestaurantPage = () => {
     updateRestaurant,
     isLoading: isUpdateLoading
   } = useUpdateMyRestaurant();
+
+  const {
+    orders,
+    isLoading: areOrdersLoading
+  } = useGetMyRestaurantOrders();
 
   const isEditing = !!restaurant;
 
@@ -40,13 +53,50 @@ const ManageRestaurantPage = () => {
   };
 
   return (
-    <div>
-      <ManageRestaurantForm
-        restaurant={restaurant as Restaurant | undefined}
-        onSave={isEditing ? handleUpdate : handleCreate}
-        isLoading={isCreateLoading || isUpdateLoading}
-      />
-    </div>
+    <Tabs defaultValue='orders'>
+      <TabsList>
+        <TabsTrigger value='orders'>
+          Orders
+        </TabsTrigger>
+
+        <TabsTrigger value='manage-restaurant'>
+          Manage Restaurant
+        </TabsTrigger>
+      </TabsList>
+
+      <TabsContent
+        value='orders'
+        className='space-y-5 bg-gray-50 p-10 rounded-lg'
+      >
+        <h2 className='text-2xl font-bold'>
+          <span className='mr-2'>
+            {orders?.length}
+          </span>
+          <span>
+            active {orders?.length === 1 ? 'order' : 'orders'}
+          </span>
+        </h2>
+
+        {areOrdersLoading && (
+          <span>Loading...</span>
+        )}
+
+        {orders?.map((order) => (
+          <OrderItemCard
+            key={order._id}
+            order={order}
+          />
+        ))}
+      </TabsContent>
+
+      <TabsContent value='manage-restaurant'>
+        <ManageRestaurantForm
+          restaurant={restaurant as Restaurant | undefined}
+          onSave={isEditing ? handleUpdate : handleCreate}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
